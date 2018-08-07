6b19e0334d836bfc62dab0f4cf0bb56a906f6dc6
Wes Harrell <wes.harrell@vuw.ac.nz>
2018-08-07 16:49:05 +1200

    added batch example

---
+++ examples/batch/myjob.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=2G
+#SBATCH --partition=main
+#SBATCH --time=3-12:00
+#SBATCH -o /home/username/project1.out
+#SBATCH -e /home/username/project1.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=me@email.com
+
+module load python/3.6.3
+python3 project1.py
