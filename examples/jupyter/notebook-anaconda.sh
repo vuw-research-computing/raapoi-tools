47e9e64956d00f31996d1c5dd3342da8e40c7fba
Wes Harrell <wes.harrell@vuw.ac.nz>
2018-11-19 10:58:09 +1300

    added jupyter examples

---
+++ examples/jupyter/notebook-anaconda.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH -c 2
+#SBATCH --mem=4G
+#SBATCH --time=2-00:00
+#SBATCH -o notebook-%A.out
+
+IP=`hostname -i`
+PORT=`shuf -i 2000-65000 -n 1`
+
+export XDG_RUNTIME_DIR=""
+$HOME/anaconda3/bin/jupyter notebook --ip=$IP --port=$PORT --no-browser
---
+++ examples/jupyter/notebook.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -c 2
+#SBATCH --mem=4G
+#SBATCH --time=2-00:00
+#SBATCH -o notebook-%A.out
+
+IP=`hostname -i`
+PORT=`shuf -i 2000-65000 -n 1`
+
+module load jupyter/notebook
+export XDG_RUNTIME_DIR=""
+jupyter-notebook --ip=$IP --port=$PORT --no-browser
