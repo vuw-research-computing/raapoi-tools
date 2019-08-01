e1453d3f0b19e2ffdcf6b169fcf37ff322ff0ab6
Wes Harrell <wes.harrell@vuw.ac.nz>
2019-08-01 13:09:12 +1200

    made mod building easier..i hope

--- admin/bin/pymod3.6-install.sh
+++ admin/bin/pymod3.6-install.sh
@@ -11,32 +11,9 @@
 #export CFLAGS="-I/home/software/apps/local/lib/libffi-3.2.1/include -L/home/software/sloan/local/lib64"
 export PIP=pip3
 #
-module load python/3.6.6
+module load python/3.6.8
 module load python/modules/3.6
 #
-$PIP install numpy --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install pandas --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install matplotlib --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install scipy --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install mpmath --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install csvkit --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install prettyplotlib --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install plotly --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install ggplot --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install Bokeh --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install PyMC --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install Scikit-learn --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install Scikit-image --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install SymPy --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install Statsmodels --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install jupyter-core --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install Scrapy --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install networkx --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install boto3 --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install gensim --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install saspy --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install pysqlite --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install jupyter --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install tabulate --target=/home/software/apps/local/lib/py36 --upgrade
-$PIP install sphinx --target=/home/software/apps/local/lib/py36 --upgrade
-
+for mod in $( cat ../etc/py36-mods.txt ); do
+  $PIP install $mod --target=/home/software/apps/local/lib/py36 --upgrade
+done
--- admin/bin/pymod3.7-install.sh
+++ admin/bin/pymod3.7-install.sh
@@ -14,27 +14,6 @@ export PIP=pip3
 module load python/3.7.0
 module load python/modules/3.7
 #
-$PIP install numpy --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install pandas --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install matplotlib --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install scipy --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install mpmath --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install csvkit --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install prettyplotlib --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install plotly --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install ggplot --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install Bokeh --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install PyMC --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install Scikit-learn --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install Scikit-image --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install SymPy --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install Statsmodels --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install jupyter-core --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install Scrapy --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install networkx --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install boto3 --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install gensim --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install saspy --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install tabulate --target=/home/software/apps/local/lib/py37 --upgrade
-$PIP install sphinx --target=/home/software/apps/local/lib/py37 --upgrade
-
+for mod in $( cat ../etc/py37-mods.txt ); do
+  $PIP install $mod --target=/home/software/apps/local/lib/py37 --upgrade
+done
