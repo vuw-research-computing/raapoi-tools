79354548db205bea8ddef26762e7321e9f4de100
Wes Harrell <wes.harrell@vuw.ac.nz>
2019-10-18 14:16:21 +1300

    added group quota

--- admin/bin/get-quota.sh
+++ admin/bin/get-quota.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 LOG=/nfs/home/cad/log/quota.log
 SCRATCH=/nfs/home/cad/log/scratch.log
+GROUP=/nfs/home/cad/log/group.log
 if [ "${HOSTNAME%%}" != "raapoi_fs01" ]; then
   echo "Must be run from raapoi_fs01"
   echo ""
@@ -8,4 +9,5 @@ if [ "${HOSTNAME%%}" != "raapoi_fs01" ]; then
 else
   sudo xfs_quota -x -c 'report -u' /mnt/home | awk '{print $1, $2, $4}' > $LOG
   sudo xfs_quota -x -c 'report -u' /mnt/scratch | awk '{print $1, $2, $4}' > $SCRATCH
+  sudo xfs_quota -x -c 'report -g' /mnt/scratch | awk '{print $1, $2, $4}' > $GROUP
 fi
