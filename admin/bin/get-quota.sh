3b43b017dcc3bc172989eb16ec44c658649c26f1
Wes Harrell <wes.harrell@vuw.ac.nz>
2020-03-17 12:02:18 +1300

    remove getquota for retired home dir, replaced by isolon homedir

--- admin/bin/get-quota.sh
+++ admin/bin/get-quota.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-LOG=/nfs/home/cad/log/quota.log
 SCRATCH=/nfs/home/cad/log/scratch.log
 SCRATCH2=/nfs/home/cad/log/scratch2.log
 GROUP=/nfs/home/cad/log/group.log
@@ -8,7 +7,6 @@ if [ "${HOSTNAME%%}" != "raapoi_fs01" ]; then
   echo ""
   exit
 else
-  sudo xfs_quota -x -c 'report -u' /mnt/home | awk '{print $1, $2, $4}' > $LOG
   sudo xfs_quota -x -c 'report -u' /mnt/scratch | awk '{print $1, $2, $4}' > $SCRATCH
   sudo xfs_quota -x -c 'report -u' /mnt/scratch2 | awk '{print $1, $2, $4}' > $SCRATCH2
   sudo xfs_quota -x -c 'report -g' /mnt/scratch | egrep -v "(^#|^$|users|root|wheel|cadadm)" | tail -n +5 | awk '{print $1, $2, $4}' > $GROUP
