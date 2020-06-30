9d892927f5bc7ac29324558db9878c6e57e34600
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2020-06-30 16:06:53 +1200

    Added no cuda version of specfem2d

---
+++ modulefiles/specfem2d/nov2018-nocuda.lua
@@ -0,0 +1,16 @@
+-- -*- lua -*-
+help(
+[[
+This module sets up specfem2d with mpi
+]])
+
+
+
+
+always_load("ucx/1.8.0","openmpi/4.0.4","gcc/8.2.0")
+
+prepend_path("PATH", "/home/software/apps/specfem2d/nov18bin")  -- /home/software/apps/specfem2d/nov18bin
+
+whatis("Name        : specfem2d-nocuda")
+whatis("Version     : Nov2018-nocuda")
+whatis("URL         : https://github.com/geodynamics/specfem2d")
