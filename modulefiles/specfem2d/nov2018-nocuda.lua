e8bdc48a6514c7aae0be61cc0b80d360321eab32
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2020-06-30 16:22:13 +1200

    typo!

--- modulefiles/specfem2d/nov2018-nocuda.lua
+++ modulefiles/specfem2d/nov2018-nocuda.lua
@@ -9,7 +9,7 @@ This module sets up specfem2d with mpi
 
 always_load("ucx/1.8.0","openmpi/4.0.4","gcc/8.2.0")
 
-prepend_path("PATH", "/home/software/apps/specfem2d/nov18bin")  -- /home/software/apps/specfem2d/nov18bin
+prepend_path("PATH", "/home/software/apps/specfem2d/nov18bin-nocuda")  -- /home/software/apps/specfem2d/nov18bin
 
 whatis("Name        : specfem2d-nocuda")
 whatis("Version     : Nov2018-nocuda")
