69cce55e1d814113f1cec705f20fd40efe233658
Wes Harrell <wes.harrell@vuw.ac.nz>
2020-10-14 15:04:51 +1300

    fixed

--- admin/bin/dump_SlurmDB.sh
+++ admin/bin/dump_SlurmDB.sh
@@ -2,13 +2,13 @@
 # Cron script to dump Slurm MariaDB to backup on Isolon
 # Should run sometime in the wee hours when less busy
 
-DUMPTMP=$(mktemp -q)
-DUMPFILE=slurmdb-$(date +%Y%m%d_%H%M).sql.gz
+DUMPTMP=$(mktemp -d -q)
+DUMPFILE=slurmdb-$(date +%Y%m%d_%H%M).sql
 BAKDIR=/nfs/home/backups/slurm
 
-mysqldump slurm > $DUMPTMP 
-gzip -q $DUMPTMP > $BAKDIR/$DUMPFILE
+mysqldump slurm > $DUMPTMP/$DUMPFILE
+gzip -q $DUMPTMP/$DUMPFILE && mv $DUMPTMP/${DUMPFILE}.gz $BAKDIR/
 
-[[ -e $BAKDIR/$DUMPFILE ]] && rm -f $DUMPTMP
+rm -rf $DUMPTMP
 
 exit 0
