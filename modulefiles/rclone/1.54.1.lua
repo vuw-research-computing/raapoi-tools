efd33fdf3c080707de57b6591adeb339d1de62dd
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2021-03-24 11:21:16 +1300

    added rclone

---
+++ modulefiles/rclone/1.54.1.lua
@@ -0,0 +1,28 @@
+help([==[
+
+Description
+===========
+Rclone is a command line program to sync files and directories to and from
+ a variety of online storage services
+
+
+More information
+================
+ - Homepage: https://rclone.org/
+]==])
+
+whatis([==[Description: 
+ Rclone is a command line program to sync files and directories to and from
+ a variety of online storage services
+]==])
+whatis([==[Homepage: https://rclone.org/]==])
+whatis([==[URL: https://rclone.org/]==])
+
+local root = "/home/software/apps/rclone/1.54.1"
+
+conflict("rclone")
+
+prepend_path("CMAKE_PREFIX_PATH", root)
+prepend_path("MANPATH", pathJoin(root, "share/man"))
+prepend_path("PATH", pathJoin(root, "bin"))
+prepend_path("XDG_DATA_DIRS", pathJoin(root, "share"))
