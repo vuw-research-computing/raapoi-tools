4e0cf4d916469742da3f89f5ed7fb5ed221b38f0
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2021-04-12 16:58:27 +1200

    Added python dpendencies and manpath

--- modulefiles/nextDenovo/2.4.0.lua
+++ modulefiles/nextDenovo/2.4.0.lua
@@ -14,7 +14,10 @@ whatis([==[URL: https://github.com/Nextomics/NextDenovo]==])
 local root = "/home/software/apps/nextDenovo/2.4.0"
 
 conflict("nextDenovo")
+always_load("python/3.8.1")
 
 prepend_path("PATH", pathJoin(root))
 prepend_path("PATH", pathJoin(root,"/bin"))
-prepend_path( "LD_LIBRARY_PATH",pathJoin(root,"/lib"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(root,"/lib"))
+prepend_path("PYTHON_PATH",pathJoin(root,"/pythondeps/lib/python3.8/site-packages"))
+prepend_path("MANPATH",pathJoin(root,"/doc")
