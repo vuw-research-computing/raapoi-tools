5bf04fc87ceebd6d56491dcaa7cac2581bbd1f30
Andre geldenhuis <andre.geldenhuis@vuw.ac.nz>
2021-04-30 12:50:23 +1200

    Added NBO7

---
+++ modulefiles/NBO/7.0.10.lua
@@ -0,0 +1,23 @@
+help([==[
+
+Description
+===========
+The Natural Bond Orbital (NBO) program
+
+
+More information
+================
+ - Homepage: https://nbo6.chem.wisc.edu/
+]==])
+
+whatis([==[Description: 
+A discovery tool for chemical insights from complex wavefunctions.
+]==])
+whatis([==[Homepage: https://nbo6.chem.wisc.edu/]==])
+whatis([==[URL: https://nbo6.chem.wisc.edu/]==])
+
+local root = "/home/software/apps/nbo/7.0.10"
+
+conflict("NBO")
+
+prepend_path("PATH", pathJoin(root,"/bin"))
