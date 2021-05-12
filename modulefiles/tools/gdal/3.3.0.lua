9f4164e91c37ce278e7866899c79c350886cdd2e
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2021-05-12 17:38:23 +1200

    Missing bin path

--- modulefiles/tools/gdal/3.3.0.lua
+++ modulefiles/tools/gdal/3.3.0.lua
@@ -37,7 +37,6 @@ prepend_path("CMAKE_PREFIX_PATH", root)
 prepend_path("CPATH", pathJoin(root, "include"))
 prepend_path("LD_LIBRARY_PATH", pathJoin(root, "lib"))
 prepend_path("LIBRARY_PATH", pathJoin(root, "lib"))
+prepend_path("PATH", pathJoin(root, "bin"))
 prepend_path("PKG_CONFIG_PATH", pathJoin(root, "lib/pkgconfig"))
 prepend_path("XDG_DATA_DIRS", pathJoin(root, "share"))
-
--- Modified from EasyBuild version 4.3.0
