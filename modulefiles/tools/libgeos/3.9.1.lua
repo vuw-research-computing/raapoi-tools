b71d20bce6b86124c0e5c703feb666f1d2b5ec1b
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2021-05-12 17:53:09 +1200

    Added libgeos

---
+++ modulefiles/tools/libgeos/3.9.1.lua
@@ -0,0 +1,31 @@
+help([==[
+
+Description
+===========
+GEOS is a C++11 library for performing operations on two-dimensional vector geometries. 
+It is primarily a port of the JTS Topology Suite Java library. It provides many of the algorithms used by PostGIS, 
+the Shapely package for Python, the sf package for R, and others.
+
+More information
+================
+ - Homepage: https://github.com/libgeos/geos
+]==])
+
+whatis([==[Description: 
+GEOS is a C++11 library for performing operations on two-dimensional vector geometries. 
+It is primarily a port of the JTS Topology Suite Java library. It provides many of the algorithms used by PostGIS, 
+the Shapely package for Python, the sf package for R, and others.
+]==])
+whatis([==[Homepage: https://github.com/libgeos/geos]==])
+whatis([==[URL: https://github.com/libgeos/geos]==])
+
+local root = "/home/software/apps/tools/libgeos/3.9.1"
+
+conflict("tools/libgeos")
+
+if not ( isloaded("gcc/8.2.0") ) then
+    load("gcc/8.2.0")
+end
+
+prepend_path("CMAKE_PREFIX_PATH", root)
+prepend_path("CPATH", pathJoin(root, "include"))
