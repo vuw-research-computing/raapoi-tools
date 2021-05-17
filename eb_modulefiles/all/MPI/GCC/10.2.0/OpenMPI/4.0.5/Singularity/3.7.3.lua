247d682421afa3e74155ca614b582e2a0d1b2c80
Andre geldenhuis <andre.geldenhuis@vuw.ac.nz>
2021-05-18 11:36:11 +1200

    Added new singularity built via easybuild toolchains

---
+++ eb_modulefiles/all/MPI/GCC/10.2.0/OpenMPI/4.0.5/Singularity/3.7.3.lua
@@ -0,0 +1,34 @@
+help([==[
+
+Description
+===========
+Singularity is a portable application stack packaging and runtime utility
+
+
+More information
+================
+ - Homepage: https://github.com/hpcng/singularity
+]==])
+
+whatis([==[Description: Singularity is a portable application stack packaging and runtime utility.]==])
+whatis([==[Homepage: https://github.com/hpcng/singularity]==])
+whatis([==[URL: https://github.com/hpcng/singularity/]==])
+
+local root = "/home/software/EasyBuild/software/Singularity/3.7.3-gompi-2020b"
+
+conflict("Singularity")
+
+if not ( isloaded("GCC/10.2.0") ) then
+    load("GCC/10.2.0")
+end
+
+if not ( isloaded("OpenMPI/4.0.5") ) then
+    load("OpenMPI/4.0.5")
+end
+
+if not ( isloaded("Go/1.14.1") ) then
+    load("Go/1.14.1")
+end
+
+
+prepend_path("PATH", root, "bin")
