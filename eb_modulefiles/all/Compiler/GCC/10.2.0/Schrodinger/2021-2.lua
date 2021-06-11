b50e09981712405c20a523a5db5337900542dd7f
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2021-06-11 17:06:03 +1200

    Added new Schrodinger

---
+++ eb_modulefiles/all/Compiler/GCC/10.2.0/Schrodinger/2021-2.lua
@@ -0,0 +1,28 @@
+help([==[
+
+More information
+================
+ - Homepage: https://www.schrodinger.com/
+]==])
+
+whatis([==[Description: 
+Adds the Schrodinger Suite v 2021-2]==])
+whatis([==[Homepage: https://www.schrodinger.com/]==])
+whatis([==[URL: https://www.schrodinger.com/==])
+
+local root = "/home/software/apps/Schrodinger/2021-2"
+
+conflict("Schrodinger")
+always_load("Python/3.8.6")
+
+module load GCCcore/10.2.0
+module load Python/3.8.6
+
+prepend_path("PATH", pathJoin(root))
+prepend_path("PATH", pathJoin(root,"/utilities"))
+prepend_path("PATH", pathJoin(root,"/thirdparty/bin/Linux-x86_64/blast/bin"))
+prepend_path("PATH", pathJoin(root,"/thirdparty/bin/Linux-x86_64/hmmer/bin"))
+prepend_path("PATH", pathJoin(root,"/utilities"))
+
+setenv("schrodinger",root)
+setenv("SCHRODINGER".root)
