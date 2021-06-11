b0bc0e20f9216e57cd47d546528e840e1d3fd776
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2021-06-11 17:15:38 +1200

    typo

--- eb_modulefiles/all/Compiler/GCCcore/10.2.0/Schrodinger/2021-2.lua
+++ eb_modulefiles/all/Compiler/GCCcore/10.2.0/Schrodinger/2021-2.lua
@@ -15,9 +15,6 @@ local root = "/home/software/apps/Schrodinger/2021-2"
 conflict("Schrodinger")
 always_load("Python/3.8.6")
 
-module load GCCcore/10.2.0
-module load Python/3.8.6
-
 prepend_path("PATH", pathJoin(root))
 prepend_path("PATH", pathJoin(root,"/utilities"))
 prepend_path("PATH", pathJoin(root,"/thirdparty/bin/Linux-x86_64/blast/bin"))
